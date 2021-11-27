74eb8b8774ccd5d671e53e5169a6cb2aeb98500b
Jaime Perez <chunkym0nkey@protonmail.com>
2021-11-27 01:29:54 -0500

    commit tree 52ebc0afa1e189da67e5c0592c849ffa76abc840

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,75 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2],
+]
+
+#won
+
+def won?(board)
+  WIN_COMBINATIONS.each do |single_win_combo|
+    win_index_1 = single_win_combo[0]
+    win_index_2 = single_win_combo[1]
+    win_index_3 = single_win_combo[2]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+   
+    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      return single_win_combo
+    end
+  end
+  return false
+end
+
+#full?
+
+def full?(board)
+  if board.any? {|index| index == nil || index == " "}
+    return false
+  else
+    return true
+  end
+end
+
+
+
+#draw?
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  elsif!full?(board) && !won?(board)
+    return false
+  else won?(board)
+    return false
+  end
+end
+
+
+#over?
+
+def over?(board)
+  if draw?(board) || won?(board) || full?(board) 
+    return true
+  else
+    return false
+  end
+ end
+
+#winner
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  end
+end
